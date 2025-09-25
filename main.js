4dbf65cd9cebea615febb80741cd47566eac6fb7
mayradelrio <mayra.delrio@utec.edu.pe>
2025-09-24 20:02:35 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var enlace_api = 'http://http://18.205.169.99:8000/'
+var enlace_api = 'http://18.205.169.99:8000/'
 
 var solicitar_lista=  (event) => {
         fetch(enlace_api + '/employees')
@@ -49,4 +49,5 @@ if(document.forms[0]){
             console.log(err);
         })  
     });
+
 }
